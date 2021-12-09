6539c8fdddeae16a6541cf35d81a8c8621674907
shehabs010 <shehabs010@gmail.com>
2021-12-09 11:53:45 +0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <ul v-for="item in example1" :key="item.id">
-      <li><router-link href="">{{item.name}}</router-link></li>
+      <li><router-link to="">{{item.name}}</router-link></li>
       
     </ul>
   </div>
